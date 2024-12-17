0740789d32a0578d64183edfd2b0570b3c2c30a2
lcx <2948065094@qq.com>
2024-12-17 13:43:23 +0800

    修改逻辑基本完善，差修改库存表，以及减少有bug

--- main_ui.py
+++ main_ui.py
@@ -10,13 +10,14 @@ event_log_path = 'files/event_log.txt'
 modify_inventory_log_path = 'files/modify_inventory_log.txt'
 users_path = 'files/users.txt'
 
+
 # 用户管理
 def load_users():
     users = {}
     if os.path.exists(f'{users_path}'):
         with open(f'{users_path}', 'r', encoding='utf-8') as file:
             for line in file:
-                id, name, password = line.strip().split(',')
+                id, name, password = line.strip().split('|@|')
                 users[id] = {'name': name, 'password': password}
     return users
 
@@ -24,7 +25,7 @@ def verify_user(id, password):
     users = load_users()
     print(users)
     if id in users and users[id]['password'].strip() == password:
-        return True
+        return [id, users[id]['name'].strip()]
     return False
 
 # 库存管理
@@ -41,39 +42,55 @@ def remove_item(item_name, quantity):
 # 修改物品操作 -> 通过此操作进行添加数量和减少数量
 def modify_item(item_name, quantity, type='add', index=0, parent=None):
     # 验证操作人员，验证通过才能进行操作
-    while not verify_user_ui(parent):
-        pass
+    verify_result = verify_user_ui(parent)
+    if not verify_result:
+        return
      
-    if   == 'add':
-        # 1、操作库存数量文件
-        # 先读取文件，再写入
-        lines = []
-        with open(f'files/kucun.txt', 'r', encoding='utf-8') as file:
-            lines = file.readlines()
-        data = [line.strip().split(',') for line in lines[1:]]
-        # # 修正格式代码
-        # data = []
-        # for i in range(1, len(lines)):
-        #     data.append(lines[i].split(','))
-        #     print(data[0])
-        #     for j in range(len(data[i-1])):
-        #         data[i-1][j] = data[i-1][j].strip()
-        # print(data)
+    # 1、操作库存数量文件
+    # 先读取文件，再写入
+    lines = []
+    with open(f'files/kucun.txt', 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+    data = [line.strip().split('|@|') for line in lines[1:]]
+    # # 修正格式代码
+    # data = []
+    # for i in range(1, len(lines)):
+    #     data.append(lines[i].split(','))
+    #     print(data[0])
+    #     for j in range(len(data[i-1])):
+    #         data[i-1][j] = data[i-1][j].strip()
+    # print(data)
+    # 判断逻辑
+    if type == 'add':
         # 修改库存数量
         data[index][2] = ' ' + str(int(data[index][2]) + int(quantity))
-        # 写入文件
-        with open(f'files/kucun.txt', 'w', encoding='utf-8') as file:
-            file.write("name, id, num\n")
-            for row in data:
-                file.write(f"{row[0]},{row[1]},{row[2]}\n")
+    elif type == 'remove':
+        # 修改库存数量
+        data[index][2] = ' ' + str(int(data[index][2]) - int(quantity))
+    
+    # 写入文件
+    with open(f'files/kucun.txt', 'w', encoding='utf-8') as file:
+        file.write("name|@| id|@| num\n")
+        for row in data:
+            file.write(f"{row[0]}|@|{row[1]}|@|{row[2]}\n")
+    if type == 'add':
         # 2、对此次操作进行记录，记录到事件操作表和修改库存表
-        with open(f'{event_log_path}', 'a') as file:
+        with open(f'{event_log_path}', 'a', encoding='utf-8') as file:
             # 记录事件操作  （事件描述, 操作员工id, 操作员工, 变更内容, 操作时间）
-            file.write(f'添加物品, {item_name}, {quantity}, {datetime.datetime.now()}\n')
-        
-    else:
-        with open(f'{item_name}.txt', 'a') as file:
-            file.write(f'{datetime.datetime.now()}, -{quantity}\n')
+            file.write(f'添加物品|@| {verify_result[0]}|@| {verify_result[1]}|@| 货物: {item_name}, 从{int(data[index][2]) - int(quantity)}修改为{data[index][2]}|@| {datetime.datetime.now()}\n')
+        with open(f'{modify_inventory_log_path}', 'a', encoding='utf-8') as file:
+            # 记录库存变更  （物品名称|@| 修改类型|@| 修改数量|@| 修改后数量|@| 操作员工姓名|@| 操作员工ID|@| 修改时间）
+            file.write(f'{item_name}|@| add|@| {quantity}|@| {data[index][2]}|@| {verify_result[1]}|@| {verify_result[0]}|@| {datetime.datetime.now()}\n')
+            
+    elif type == 'remove':
+        # 2、对此次操作进行记录，记录到事件操作表和修改库存表
+        with open(f'{event_log_path}', 'a', encoding='utf-8') as file:
+            # 记录事件操作  （事件描述, 操作员工id, 操作员工, 变更内容, 操作时间）
+            file.write(f'减少物品|@| {verify_result[0]}|@| {verify_result[1]}|@| 货物: {item_name}, 从{int(data[index][2]) + int(quantity)}修改为{data[index][2]}|@| {datetime.datetime.now()}\n')
+        with open(f'{modify_inventory_log_path}', 'a', encoding='utf-8') as file:
+            # 记录库存变更  （物品名称|@| 修改类型|@| 修改数量|@| 修改后数量|@| 操作员工姓名|@| 操作员工ID|@| 修改时间）
+            file.write(f'{item_name}|@| remove|@| {quantity}|@| {data[index][2]}|@| {verify_result[1]}|@| {verify_result[0]}|@| {datetime.datetime.now()}\n')
+
 
 # 统计功能
 def generate_report(item_name, period='daily'):
@@ -82,7 +99,7 @@ def generate_report(item_name, period='daily'):
     
     report = {}
     for line in lines:
-        date_str, change = line.strip().split(', ')
+        date_str, change = line.strip().split('|@| ')
         date = datetime.datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S.%f')
         
         if period == 'daily':
@@ -100,18 +117,27 @@ def generate_report(item_name, period='daily'):
     
     return report
 
-# 验证用户
+# 验证用户->弹出验证用户信息对话框,成功返回用户信息,失败返回False
 def verify_user_ui(parent):
     dialog_verify = VerifyDialog(parent,title="验证")
     # dialog_verify.geometry("500x150")
     user_id, password = dialog_verify.get_credentials()
-    
-    if verify_user(user_id, password):
-        messagebox.showinfo("成功", "用户验证成功,已修改库存")
-        return True
-    else:
+
+    # 返回信息
+    verify_result = verify_user(user_id, password)
+    if not verify_result:
         messagebox.showerror("错误", "用户验证失败")
         return False
+    else:
+        messagebox.showinfo("成功", "用户验证成功")
+        return verify_result
+    
+    # if verify_user(user_id, password):
+    #     messagebox.showinfo("成功", "用户验证成功,已修改库存")
+    #     return True
+    # else:
+    #     messagebox.showerror("错误", "用户验证失败")
+    #     return False
 
 # 自定义验证用户信息对话框
 class VerifyDialog(simpledialog.Dialog):
@@ -141,12 +167,19 @@ class VerifyDialog(simpledialog.Dialog):
 class InventoryApp:
     def __init__(self, root):
 
+        # 获取屏幕宽度和高度
+        self.screen_width = root.winfo_screenwidth()
+        self.screen_height = root.winfo_screenheight()
+
         # 库存
         self.kucun = None
 
         self.root = root
         self.root.title("寺庙库存管理系统")
-        self.root.geometry("800x500")
+        # self.root.geometry("800x500")
+        x = (self.screen_width - 800) // 2
+        y = (self.screen_height - 500) // 2
+        self.root.geometry(f"800x500+{x}+{y}")
         
         self.menu_frame = tk.Frame(root)
         self.menu_frame.pack(side=tk.TOP, fill=tk.X)
@@ -235,7 +268,7 @@ class InventoryApp:
 
         # headers = lines[0].strip().split(',')
         headers = ['物品名称', '库存数量']
-        data = [line.strip().split(',') for line in lines[1:]]
+        data = [line.strip().split('|@|') for line in lines[1:]]
         # 库存更新
         self.kucun = data
 
@@ -295,7 +328,10 @@ class InventoryApp:
         # 弹出新窗口
         self.dialog_inventory = tk.Toplevel(self.root)
         self.dialog_inventory.title("库存管理")
-        self.dialog_inventory.geometry("300x150")
+        # self.dialog_inventory.geometry("300x150")
+        x = (self.screen_width - 300) // 2
+        y = (self.screen_height - 150) // 2
+        self.dialog_inventory.geometry(f"300x150+{x}+{y}")
         # 设置关闭事件
         self.dialog_inventory.protocol("WM_DELETE_WINDOW", self.on_closing_dialog_inventory)
         # # 设置弹框顶置
@@ -321,24 +357,44 @@ class InventoryApp:
         quantity_entry = tk.Entry(self.dialog_inventory)
         quantity_entry.grid(row=2, column=1, padx=10, pady=5, sticky="w")
         quantity_entry.config(width=10)
+        # 默认值为1
+        quantity_entry.insert(0, "1")
+        # 只能输入数字
+
+        # 离开焦点事件
+        # quantity_entry.bind('<FocusOut>',lambda e: self.on_focus_out_inventory_event(product_quantity, e))
         
         # 第四行加入空白标签 (如果需要)
         tk.Label(self.dialog_inventory, text="").grid(row=3, column=0, columnspan=2)
         
         # 添加按钮
-        tk.Button(self.dialog_inventory, text="添加", command=lambda: self.add_product(product_name, quantity_entry.get(), selected_index)).grid(row=4, column=0, padx=10, pady=10, sticky="ew")
+        tk.Button(self.dialog_inventory, text="添加", command=lambda: self.add_product(product_name, product_quantity, quantity_entry.get(), selected_index)).grid(row=4, column=0, padx=10, pady=10, sticky="ew")
         
         # 减少按钮
-        tk.Button(self.dialog_inventory, text="减少", command=lambda: self.remove_product(product_name, quantity_entry.get(), selected_index)).grid(row=4, column=1, padx=10, pady=10, sticky="ew")
+        tk.Button(self.dialog_inventory, text="减少", command=lambda: self.remove_product(product_name, product_quantity, quantity_entry.get(), selected_index)).grid(row=4, column=1, padx=10, pady=10, sticky="ew")
     
-    # 添加产品数量,index为选中的行(从0开始),便于后续修改
-    def add_product(self, product_name, quantitym, index):
+    # 添加产品数量,index为选中的行(从0开始),便于后续修改; 参数解释: product_name:产品名称, product_quantity:产品数量, quantitym:操作数量, index:选中行
+    def add_product(self, product_name, product_quantity, quantitym, index):
+        # 验证逻辑是否合法
+        if not self.logic_verify(product_quantity, quantitym, 'add'):
+            return
         # 添加产品数量
         modify_item(product_name, quantitym, "add", index, parent=self.root)
         # 刷新页面
         self.show_inventory_management()
         # 关闭弹窗
         self.on_closing_dialog_inventory()
+    
+    # 减少产品数量
+    def remove_product(self, product_name, product_quantity, quantitym, index):
+        # 验证逻辑是否合法
+        if not self.logic_verify(product_quantity, quantitym, 'remove'):
+            return
+        # 减少产品数量
+        modify_item(product_name, quantitym, "remove", index, parent=self.root)
+        # 刷新页面
+        self.clear_inventory_frame()
+        self.create_inventory_management()
 
     # 监听dialog_inventory窗口的关闭事件
     def on_closing_dialog_inventory(self):
@@ -349,15 +405,33 @@ class InventoryApp:
         self.dialog_inventory.destroy()
         # root取消顶层
         self.root.attributes("-topmost", False)
+
+    # 修改库存逻辑验证
+    def logic_verify(self, product_quantity, quantitym, type):
+        print("逻辑验证")
+        # 1、验证quantitym是否为数字
+        if not quantitym.isdigit():
+            messagebox.showerror("错误", "请输入数字")
+            return False
+        # 2、验证quantitym是否大于0
+        if int(quantitym) <= 0:
+            messagebox.showerror("错误", "请输入大于0的数字")
+            return False
+        # 3、验证type是否为add或remove
+        if type == 'remove':
+            if int(quantitym) > int(product_quantity):
+                messagebox.showerror("错误", "减少数量大于库存数量")
+                return False
+        return True
+            
         
-    
-    # 减少产品数量
-    def remove_product(self, product_name, quantity):
-        # 减少产品数量
-        remove_item(product_name, quantity)
-        # 刷新页面
-        self.clear_inventory_frame()
-        self.create_inventory_management()
+        # if not event.widget.get().isdigit():
+        #     messagebox.showerror("错误", "请输入数字")
+        #     event.widget.delete(0, tk.END)
+        #     event.widget.insert(0, 1)
+        
+        # content = event.widget.get()
+        
     
     # 清空库存物品
     def clear_inventory_frame(self):
